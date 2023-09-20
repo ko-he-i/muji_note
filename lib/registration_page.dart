81e050fdaf36b2521cf9321f4689988c858b3de1
ko-he-i <ogiharakohei@gmail.com>
2023-09-20 18:48:49 +0900

    新規登録ページ作成中

--- lib/registration_page.dart
+++ lib/registration_page.dart
@@ -10,6 +10,10 @@ class RegistrationPage extends StatefulWidget {
 class _RegistrationPageState extends State<RegistrationPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('data'),
+      ),
+    );
   }
 }
