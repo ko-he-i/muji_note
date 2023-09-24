874b1a233aefc2efd92c6560032831b43c86f4c7
ko-he-i <ogiharakohei@gmail.com>
2023-09-25 08:33:18 +0900

    アイコン

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -22,10 +22,15 @@ class _StartPageState extends State<StartPage> {
             Text(
               'Muji Memo',
               style: GoogleFonts.notoSansJp(
-                fontSize: 40, // フォントサイズ
-                fontWeight: FontWeight.bold, // フォントの太さ
-                color: Color.fromARGB(255, 237, 95, 95), // フォントの色
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 237, 95, 95),
               ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: Image.asset('lib/assets/images/muji_note_logo_1024px.PNG'),
             ),
             SizedBox(
               width: 200,
